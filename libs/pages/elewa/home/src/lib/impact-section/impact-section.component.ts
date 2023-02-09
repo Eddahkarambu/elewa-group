cde72b9f4339c7717142cae3d0436ebd4af8a301
Eddahkarambu <gikundaeddah@gmail.com>
2023-02-09 16:33:41 +0300

    :feat  create impact section

---
+++ libs/pages/elewa/home/src/lib/impact-section/impact-section.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'elewa-group-impact-section',
+  templateUrl: './impact-section.component.html',
+  styleUrls: ['./impact-section.component.scss'],
+})
+export class ImpactSectionComponent {}
