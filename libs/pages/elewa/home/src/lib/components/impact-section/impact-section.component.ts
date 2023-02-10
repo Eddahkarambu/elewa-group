c453182a9f1d658cfd233fe4c30c8077b2bfc468
Eddahkarambu <gikundaeddah@gmail.com>
2023-02-10 11:49:22 +0300

    feat:add stylying  to the image

--- libs/pages/elewa/home/src/lib/components/impact-section/impact-section.component.ts
+++ libs/pages/elewa/home/src/lib/components/impact-section/impact-section.component.ts
@@ -6,10 +6,10 @@ import { Component,Input } from '@angular/core';
   styleUrls: ['./impact-section.component.scss'],
 })
 export class ImpactSectionComponent {
-  @Input() title1 = 'Why do we use it?'
-  @Input() title2 = 'Where can I get some?'
-  @Input() paragraph1 = 'printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
-  @Input() paragraph2 = 'Where can I get some?'
+  @Input() title1 = 'Growing capital'
+  @Input() title2 = 'Inventing cives?'
+  @Input() paragraph1 = 'Elewa is proof that growing capital and empacting lives can work well together'
+  @Input() paragraph2 = 'Our group owned and managed by our founder, our employees(across our brands)our network of 56 passionate angel investors'
 
   @Input() imgSrc = "https://images.unsplash.com/photo-1534982841079-afde227ada8f?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fHNtYWxsJTIwc2Nob29sJTIwa2lkcyUyMGltYWdlc3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=1400&q=60"
 }
