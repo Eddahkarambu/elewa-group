e8a85ea60afd05e648c14db9dcb4165be0385d42
Eddahkarambu <gikundaeddah@gmail.com>
2023-02-14 12:03:59 +0300

    feat:add social impact compoent

---
+++ libs/pages/elewa/social-impact/src/lib/pages-elewa-social-impact.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SocialImpactComponent } from './components/social-impact/social-impact.component';
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [SocialImpactComponent],
+})
+export class PagesElewaSocialImpactModule {}
